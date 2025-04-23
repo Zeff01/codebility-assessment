d335a27f303d536c012753f9f238dc72d25675da
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2025-04-23 13:37:43 +0800

    refactor: todo.controller.ts

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -5,28 +5,20 @@ import todoService from "../service/todo.service";
 class TodosController {
 
     getAllTodos = (req: Request, res: Response, next: NextFunction) => {
-        try {
             const todoList = todoService.getAllTodos();
             return res.status(200).json({
                 success: true,
                 data: todoList
             });
-        } catch (err) {
-            next(err);
-        }
     };
 
     getOneTodo = (req: Request, res: Response, next: NextFunction) => {
-        try {
             const id = Number(req.params.id);
             const todo = todoService.getOnetodo(id);
             return res.status(200).json({
                 success: true,
                 data: todo
             });
-        } catch (err) {
-            next(err);
-        }
     };
 
     createNewTodo = (req: Request, res: Response, next: NextFunction) => {
@@ -43,22 +35,26 @@ class TodosController {
     };
 
     updateTodo = (req: Request, res: Response, next: NextFunction) => {
-        const id = Number(req.params.id);
-        const todo = newTodoDto.parse(req.body);
-        const updatedTodo = todoService.updateTodo(id, todo);
-        return res.status(200).json({
-            success: true,
-            data: updatedTodo
-        });
+        try {
+            const id = Number(req.params.id);
+            const todo = newTodoDto.parse(req.body);
+            const updatedTodo = todoService.updateTodo(id, todo);
+            return res.status(200).json({
+                success: true,
+                data: updatedTodo
+            });
+        } catch (err) {
+            next(err);
+        }
     };
 
     deleteTodo = (req: Request, res: Response, next: NextFunction) => {
-        const id = Number(req.params.id);
-        const currentTodoList = todoService.deleteTodo(id);
-        return res.status(200).json({
-            success: true,
-            message: `Todo Id ${id} successfully deleted`
-        });
+            const id = Number(req.params.id);
+            const currentTodoList = todoService.deleteTodo(id);
+            return res.status(200).json({
+                success: true,
+                message: `Todo Id ${id} successfully deleted`
+            });
     };
 }
 
