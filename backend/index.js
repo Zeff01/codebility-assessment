e7ca9d99d8fd2cf8d57348aef573b8ac82e34032
raineer <raineerdelarita@gmail.com>
2025-04-24 13:05:06 +0800

    ne Date ()

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const itemsRouter = require('./routes/items');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/items', itemsRouter);
+
 // Basic route
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const data = [
-{id: 1, title: 'Wash dishes', completed: false, createdOn: newDate() },
-{id: 2, title: 'Wash dishes', completed: false, createdOn: newDate() },
-{id: 3, title: 'Wash dishes', completed: false, createdOn: newDate() },
-{id: 4, title: 'Wash dishes', completed: false, createdOn: newDate() },
-{id: 5, title: 'Wash dishes', completed: false, createdOn: newDate() },
+{id: 1, title: 'Wash dishes', completed: false, createdOn: new Date() },
+{id: 2, title: 'Wash dishes', completed: false, createdOn: new Date() },
+{id: 3, title: 'Wash dishes', completed: false, createdOn: new Date() },
+{id: 4, title: 'Wash dishes', completed: false, createdOn: new Date() },
+{id: 5, title: 'Wash dishes', completed: false, createdOn: new Date() },
 ];
 
 router.get('/',(req, res) => {
-
+    try {
+        
+    } catch (error) {
+        
+    }
 });
