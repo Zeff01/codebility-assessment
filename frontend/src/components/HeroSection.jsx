00d56dbeaf4a069bb1ee8132c0e923506a58e302
raineer <raineerdelarita@gmail.com>
2025-04-25 00:55:03 +0800

    tailwind css

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+// src/components/HeroSection.jsx
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1>
+                Smarter Money
+            </h1>
+        </section>
+    )
+};
+
+export default HeroSection;
