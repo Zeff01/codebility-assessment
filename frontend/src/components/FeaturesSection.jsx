c23bbc9aa3b9551fdf994519cbd55ff11e82d8f9
raineer <raineerdelarita@gmail.com>
2025-04-25 01:11:49 +0800

    featureSection

--- frontend/src/components/FeaturesSection.jsx
+++ frontend/src/components/FeaturesSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FeatureSection = () => {
     return (
-        <section>
+        <section className="py-20 px-6 bg-white">
             <div>
                 <h2>
                     Hello
