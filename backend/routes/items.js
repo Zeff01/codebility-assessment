9e2e9885198c57317f1b2a3ea198916723ee9723
raineer <raineerdelarita@gmail.com>
2025-04-24 13:51:25 +0800

    task json

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const data = [
 {id: 1, title: 'Wash dishes', completed: false, createdOn: new Date() },
-{id: 2, title: 'Wash dishes', completed: false, createdOn: new Date() },
-{id: 3, title: 'Wash dishes', completed: false, createdOn: new Date() },
-{id: 4, title: 'Wash dishes', completed: false, createdOn: new Date() },
-{id: 5, title: 'Wash dishes', completed: false, createdOn: new Date() },
+{id: 2, title: 'Clean the room', completed: false, createdOn: new Date() },
+{id: 3, title: 'Brush my teeth', completed: false, createdOn: new Date() },
+{id: 4, title: 'Finish my homework', completed: false, createdOn: new Date() },
+{id: 5, title: 'Go to church', completed: false, createdOn: new Date() },
 ];
 
 router.get('/',(req, res) => {
