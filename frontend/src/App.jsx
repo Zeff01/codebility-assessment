f61e000b6a71340d9019d48ee432bab0e544274e
raineer <raineerdelarita@gmail.com>
2025-04-25 01:04:36 +0800

    featureSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeaturesSection";
 
 function App() {
   return (
     <div className="font-sans text-gray-800">
  <HeroSection />
+ <FeatureSection />
     </div>
    
   )
---
+++ frontend/src/components/FeaturesSection.jsx
@@ -0,0 +1,18 @@
+// src/components/FeaturesSection.jsx
+import React from "react";
+
+const FeatureSection = () => {
+    return (
+        <section>
+            <div>
+                <h2>
+                    Hello
+                </h2>
+            </div>
+        </section>
+    )
+}
+
+
+
+export default FeatureSection;
