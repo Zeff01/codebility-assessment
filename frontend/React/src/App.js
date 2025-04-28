2669bb859f34fd6d6b9045b53d530f74b4f719dd
Russell Sean <russell.sean.rs@gmail.com>
2025-04-28 16:35:48 +0800

    Implement landing page design in React

---
+++ frontend/React/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./Components/hero/Hero";
+
+function App() {
+  return (
+    <div>
+      <section>
+        <Hero></Hero>
+      </section>
+    </div>
+  );
+}
+
+export default App;
