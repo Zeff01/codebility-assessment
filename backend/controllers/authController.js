b9acdcf4b4e1f84d63b2fc7f0659458b4d5237a0
raineer <raineerdelarita@gmail.com>
2025-04-29 21:02:04 +0800

    router post register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,8 +7,13 @@ const bcrypt = require('bcryptjs');
 dotenv.config();
 const users = [];
 
-router.post('/register', (req, res) => {
 
+//register
+router.post('/register', async (req, res) => {
+    const { username, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 8);
+    users.push({ username, password: hashedPassword});
+    res.json({ message: 'User registered successfully'})
 });
 
 router.post('/login', (req, res) => {
