52297e3cb3b9a9c44a423777e7aa110720828c7d
raineer <raineerdelarita@gmail.com>
2025-04-29 20:54:29 +0800

    getweather service

--- backend/services/weatherService.js
+++ backend/services/weatherService.js
@@ -7,5 +7,11 @@ async function getWeather(city) {
     const apiKey = process.env.WEATHER_API_KEY;
     const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-    )
+    );
+    return {
+        temp: response.data.main.temp,
+        description: response.data.weather[0].description,
+    };
 }
+
+module.exports = {getWeather};
