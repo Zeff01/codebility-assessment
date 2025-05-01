fa668f349af6fa095118803f78f7a8edd3d7bb37
raineer <raineerdelarita@gmail.com>
2025-05-01 20:56:55 +0800

    pages post [id]

--- fullstack/src/pages/index.jsx
+++ fullstack/src/pages/index.jsx
@@ -1,23 +1,28 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/posts')
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
+    <div className='max-w-2xl mx-auto p-4'>
+      <h1 className="text-3xl font-bold mb-6">Blog</h1>
+      {posts.map((post)=> (
+        <div key={post.id} className="mb-6 border rounded-xl hover:shadow">
+          <Link href={`/posts/${post.id}`} className='text-xl font-semibold text-blue-600 hover:underline'>
+          {post.title}
+          </Link>
+          <p className="text-gray-600">{post.excerpt}</p>
+          <p className="text-gray-400">{post.date}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ fullstack/src/pages/posts/[id].jsx
@@ -0,0 +1,3 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
