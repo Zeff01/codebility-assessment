40b8eee06ef18d9da3bc52257a5f56abcc84dd46
raineer <raineerdelarita@gmail.com>
2025-05-01 20:58:31 +0800

    post detail

--- fullstack/src/pages/posts/[id].jsx
+++ fullstack/src/pages/posts/[id].jsx
@@ -1,3 +1,11 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+
+export default function PostDetail() {
+    return (
+        <div>
+            <h1>Post Detail</h1>
+        </div>
+    )
+}
