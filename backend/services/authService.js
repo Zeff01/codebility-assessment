8cc374461e2c7ef20132b46f4cc7c455286ee5bc
raineer <raineerdelarita@gmail.com>
2025-05-03 01:30:56 +0800

    services autth service login register

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,3 +1,25 @@
 const jwt = require('jsonwebtoken');
 const { createUser, findUserByUsername } = require('../repositories/userRepository');
 const SECRET_KEY = process.env.JWT_SECRET;
+
+function register(username, password, role = 'user') {
+    if(findUserByUsername(username)) {
+        const err = new Error('User already exists');
+        err.status = 400;
+        throw err;
+    }
+    return createUser({username, password, role});
+}
+
+function login(username, password) {
+    const user = findUserByUsername(username);
+    if (!user || user.password !== password) {
+        const err = new Error('Invalid credentials');
+        err.status = 401;
+        throw err;
+    }
+    const token = jwt.sign({username, role: user.role}, SECRET_KEY, { expiresIn: '1h'});
+        return { token };
+}
+
+module.exports = { register, login};
