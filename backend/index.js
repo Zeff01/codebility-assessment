b6daf6b1a5aaa652e89c0c2b219267ab928666e0
raineer <raineerdelarita@gmail.com>
2025-05-06 13:34:01 +0800

    authRoutes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const authRoutes = require('./controllers/authController');
-const notesRoutes = require('./controllers/notesController');
+const notesRoutes = require('./controllers/noteController');
 
 
 const app = express();
